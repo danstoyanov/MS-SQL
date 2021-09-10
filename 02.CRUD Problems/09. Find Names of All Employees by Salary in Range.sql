8c24c4f4686b45f287babd68473a638b7f4e35a6
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-10 13:44:31 +0300

    update

--- 02.CRUD Problems/09. Find Names of All Employees by Salary in Range.sql
+++ 02.CRUD Problems/09. Find Names of All Employees by Salary in Range.sql
@@ -1,2 +1,7 @@
+-- First
 SELECT FirstName, LastName, JobTitle FROM Employees
        WHERE Salary >= 20000 AND Salary <= 30000
+
+-- Second
+SELECT FirstName, LastName, JobTitle FROM Employees
+       WHERE Salary BETWEEN 20000 AND 30000
