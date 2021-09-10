96ff5aeb38cbeba5f735feb1ea91288f9daae217
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-10 14:19:43 +0300

    update

--- 02.CRUD Problems/14. Find All Employees Except Marketing.sql
+++ 02.CRUD Problems/14. Find All Employees Except Marketing.sql
@@ -1,3 +1,10 @@
+-- first
 SELECT FirstName, LastName 
        FROM Employees
 	   WHERE DepartmentID <> 4
+
+
+-- second
+SELECT FirstName, LastName 
+       FROM Employees
+	   WHERE NOT DepartmentID = 4
