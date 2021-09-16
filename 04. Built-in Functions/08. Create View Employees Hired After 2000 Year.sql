6fe7aef6abfb05ec1bd3527b4b72d84656c2fc52
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-16 17:03:20 +0300

    update

--- 04. Built-in Functions/08. Create View Employees Hired After 2000 Year.sql
+++ 04. Built-in Functions/08. Create View Employees Hired After 2000 Year.sql
@@ -1,3 +1,10 @@
 CREATE VIEW [V_EmployeesHiredAfter2000] AS
 SELECT FirstName, LastName FROM Employees 
 	WHERE HireDate BETWEEN '2001/01/01' AND '2005/12/31'
+
+
+-- second
+CREATE VIEW [V_EmployeesHiredAfter2000] AS
+	SELECT FirstName, LastName
+	FROM Employees 
+	WHERE DETEPART(YEAR, HireDate) > 2000
