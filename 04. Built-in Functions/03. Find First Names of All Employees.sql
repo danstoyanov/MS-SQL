5dd3ec03fca6eefcaa0a82e537a623656bff7603
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-16 16:42:36 +0300

    update

--- 04. Built-in Functions/03. Find First Names of All Employees.sql
+++ 04. Built-in Functions/03. Find First Names of All Employees.sql
@@ -1,3 +1,3 @@
-SELECT FirstName, DepartmentID, HireDate FROM Employees
-	WHERE DepartmentID = 10 OR DepartmentID = 3
-	AND HireDate BETWEEN '1995/01/01' AND '2005/12/31'
+	SELECT FirstName FROM Employees
+	WHERE (DepartmentID = 10 OR DepartmentID = 3)
+	AND (HireDate BETWEEN '1995/01/01' AND '2005/12/31')
