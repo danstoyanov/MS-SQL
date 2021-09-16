75c90f514efbee8bdb7778becef28f7623c0c3b1
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-16 16:55:25 +0300

    update

--- 04. Built-in Functions/06. Find Towns Starting With.sql
+++ 04. Built-in Functions/06. Find Towns Starting With.sql
@@ -2,3 +2,9 @@ SELECT TownID, [Name] FROM Towns
 	WHERE [Name] LIKE 'B%' OR [Name] LIKE 'M%'
 	OR [Name] LIKE 'K%' OR [Name] LIKE 'E%'
 	ORDER BY [Name]
+
+-- solve with regex
+
+SELECT TownID, [Name] FROM Towns
+	WHERE[Name] LIKE '[MKBE]%'
+	ORDER BY [Name]
