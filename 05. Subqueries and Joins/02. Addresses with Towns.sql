8ec3b8863df929dde8d4813334d8ef498132a68d
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-17 17:57:00 +0300

    update

---
+++ 05. Subqueries and Joins/02. Addresses with Towns.sql
@@ -0,0 +1,5 @@
+SELECT TOP (50) e.FirstName, e.LastName, t.[Name] AS Town, a.AddressText
+	FROM Employees e
+	INNER JOIN [Addresses] a ON e.AddressID = a.AddressID
+	INNER JOIN [Towns] t ON a.TownID = t.TownID
+	ORDER BY e.FirstName ASC, e.LastName
