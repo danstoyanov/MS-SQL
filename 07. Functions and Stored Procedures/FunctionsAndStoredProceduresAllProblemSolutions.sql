9267af1b6730b21200fbe2d088f94d536fd0307b
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-23 14:19:02 +0300

    update

--- 07. Functions and Stored Procedures/FunctionsAndStoredProceduresAllProblemSolutions.sql
+++ 07. Functions and Stored Procedures/FunctionsAndStoredProceduresAllProblemSolutions.sql
@@ -90,3 +90,87 @@ GO
 EXEC usp_EmployeesBySalaryLevel 'high'
 EXEC usp_EmployeesBySalaryLevel 'Low'
 
+-- problem 7 
+CREATE OR ALTER FUNCTION ufn_IsWordComprised(@SetOfLetters VARCHAR(MAX), @Word VARCHAR(MAX))
+RETURNS BIT 
+AS
+BEGIN
+		DECLARE @Counter INT = 1;
+
+		WHILE (@Counter <= LEN(@Word))
+		BEGIN
+			DECLARE @currentLetter CHAR(1) = SUBSTRING(@Word, @Counter, 1) 
+
+			IF (CHARINDEX (@currentLetter, @SetOfLetters) = 0)
+				RETURN 0
+
+			SET @Counter += 1
+		END
+	RETURN 1;
+END
+
+-- test
+SELECT 
+	dbo.ufn_IsWordComprised('oistmiahf', 'Sofia'),
+	dbo.ufn_IsWordComprised('oistmiahf', 'halves'),
+	dbo.ufn_IsWordComprised('bobr', 'Rob'),
+	dbo.ufn_IsWordComprised('pppp', 'Guy') 
+
+--- problem 8 --- BONUS !!!
+
+--- problem 9
+CREATE OR ALTER PROC usp_GetHoldersFullName
+AS
+	SELECT (FirstName + ' ' + LastName) AS [Full Name]
+		FROM AccountHolders
+GO
+
+--- test
+EXEC usp_GetHoldersFullName
+
+--- problem 10
+CREATE OR ALTER PROC usp_GetHoldersWithBalanceHigherThan (@InputValue DECIMAL(15, 2) )
+AS
+	SELECT FirstName, LastName
+		FROM AccountHolders AS ah
+		JOIN Accounts AS ac ON ah.Id = ac.AccountHolderId
+		GROUP BY FirstName, LastName
+		HAVING SUM(Balance) > @InputValue
+		ORDER BY FirstName, LastName 
+GO
+
+--- tests
+EXEC usp_GetHoldersWithBalanceHigherThan 30000
+EXEC usp_GetHoldersWithBalanceHigherThan 31312
+
+--- problem 11
+CREATE OR ALTER FUNCTION ufn_CalculateFutureValue (@sum DECIMAL(15,4), @rate FLOAT, @years INT)
+RETURNS DECIMAL(15,4)
+AS
+BEGIN
+	DECLARE @money DECIMAL(15,4);
+
+	SET @money = @sum * POWER((1.0 + @rate), @years)
+
+	RETURN @money
+END
+
+SELECT dbo.ufn_CalculateFutureValue(1000, 0.1, 5)
+
+--- problem 12
+CREATE OR ALTER PROC usp_CalculateFutureValueForAccount (@accountId INT, @iRate FLOAT)
+AS
+	SELECT 
+		ac.Id, 
+		ah.FirstName, 
+		ah.LastName,
+		ac.Balance,
+		dbo.ufn_CalculateFutureValue(ac.Balance, @iRate, 5) AS [Balance in 5 years]
+		FROM AccountHolders ah
+		JOIN Accounts ac ON ac.AccountHolderId = ah.Id
+		WHERE ac.Id = @accountId
+GO
+
+-- tests
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
+EXEC usp_CalculateFutureValueForAccount 3, 0.8
